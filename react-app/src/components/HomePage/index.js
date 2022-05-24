ded43ec8dbb75dc6cd5e2ce135b702d3c55d550d
justin chau <jchau623@gmail.com>
2022-05-24 18:01:37 -0400

    leftnav

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,11 +1,15 @@
 import './HomePage.css'
 import { useSelector } from "react-redux";
+import LeftNav from '../LeftNav';
 
 export default function HomePage() {
 
     const sessionUser = useSelector(state => state?.session?.user)
 
     return (
+        <div className='homepage-container'>
+        <LeftNav />
         <h1>My Home Page</h1>
+        </div>
     )
 }
---
+++ react-app/src/components/LeftNav/index.js
@@ -0,0 +1,35 @@
+import React, {useEffect, useState} from 'react';
+import { NavLink } from 'react-router-dom';
+import './LeftNav.css'
+
+export default function LeftNav() {
+
+    const body = document.querySelector('body')
+    body.style.margin = '0'
+
+    const [isHomePage, setIsHomePage] = useState(false)
+
+    // console.log(window.location.href);
+    // If the url is set to '/', set isHomePage to true
+
+
+    useEffect(() => {
+        if (window.location.href === 'http://localhost:3000/' || window.location.href === 'http://localhost:3000') {
+            setIsHomePage(true)
+
+        } else {
+            setIsHomePage(false)
+        }
+    }, []);
+
+    return (
+        <div className='leftnav-container'>
+            <NavLink to='/' exact={true} activeClassName='active' className='navlink'>
+            <div className={`leftnav-home ${isHomePage ? 'highlighted' : ''}`} >
+            <i class="fa-solid fa-house" />
+            Home
+            </div>
+            </NavLink>
+        </div>
+    )
+}
