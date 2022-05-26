ccb4b66308e82c4fec42dae104657d8c26e6c373
justin chau <jchau623@gmail.com>
2022-05-26 14:38:46 -0400

    err handling

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -12,6 +12,10 @@ def user_exists(form, field):
     if user:
         raise ValidationError('Email address is already in use.')
 
+def email_length(form, field):
+    length_email = field.data
+    if len(length_email) > 80:
+        raise ValidationError("Email must be less than 80 characters")
 
 # def username_exists(form, field):
 #     # Checking if username is already in use
@@ -20,11 +24,24 @@ def user_exists(form, field):
 #     if user:
 #         raise ValidationError('Username is already in use.')
 
+def repeat_password(form, field):
+    repeat_password = field.data
+    password = form.data['password']
+
+    if not repeat_password == password:
+        raise ValidationError("Your passwords must match")
+
+def password_length(form, field):
+    password = field.data
+    if len(password) < 5 or len(password) > 20:
+        raise ValidationError('Password must be between 5 and 20 characters')
+
 
 class SignUpForm(FlaskForm):
     # username = StringField(
     #     'username', validators=[DataRequired(), username_exists])
     first_name = StringField('first_name', validators=[DataRequired('Please enter your first name')])
     last_name = StringField('last_name', validators=[DataRequired('Please enter your last name')])
-    email = StringField('email', validators=[DataRequired('Please enter your email'), user_exists])
-    password = StringField('password', validators=[DataRequired('Please enter your password')])
+    email = StringField('email', validators=[DataRequired('Please enter an email'), user_exists, email_length, Email(message='This is not a valid email')])
+    password = StringField('password', validators=[DataRequired('Please enter a password'), password_length])
+    repeat_password = StringField('repeat_password', validators=[DataRequired('Your passwords must match'), repeat_password])
